e423f9ff891c0a49dff4502f5c02240785f8d9fc
Luther Isaboke <lusaboke@gmail.com>
2024-06-26 18:37:06 +0300

    removed board

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import random
-print(input("press enter to roll dice"))
+def roll_dice():
+    print(input("press enter to roll dice"))
 
-dice1= random.randint(1,6)
-print(f'dice 1 rolled a:{dice1}')
-dice2=random.randint(1,6)
-print(f'dice 2 rolled a:{dice2}')
-steps_moved= dice1 + dice2
+    dice1= random.randint(1,6)
+    print(f"dice 1 rolled a:{dice1}")
+    dice2=random.randint(1,6)
+    print(f"dice 2 rolled a:{dice2}")
+    steps_moved= dice1 + dice2
 
-print(f"dice rolled:{steps_moved}")
+    print(f"dice rolled:{steps_moved}")
