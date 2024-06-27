03cc786cb3cd7e1d108462a6c07e841960ef4d2d
Luther Isaboke <lusaboke@gmail.com>
2024-06-27 08:59:07 +0300

    empty

--- game.py
+++ game.py
@@ -1,22 +1,21 @@
 import random
 from board import *
 from player import *
-def rollDice():
 
-    input("press enter to roll the die")
+
+
 
 dice1 = random.randint(1,6)
-dice2 = random.randint(1,6)
-
 print(f"player rolled :{dice1}")
+dice2 = random.randint(1,6)
 print(f"player rolled :{dice2}")
+
 steps_moved = dice1 + dice2 
 print(f"steps moved :{steps_moved}")
 
-def move(player, roll):
-        # Move the player based on the dice roll and update position on the board.
-        steps_moved = player.position 
-        player.position = (player.position + roll) % len(board)
-        print(f"{player.token} landed on {board[player.position]['name']}")
+#def move(player, roll):
+#        # Move the player based on the dice roll and update position on the board.
+#        player.position = (player.position + roll) % len(board)
+#        print(f"{player.token} landed on {board[player.position]['name']}")
 
 
